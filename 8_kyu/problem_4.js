578de472af0b212b2b0d6142a92de52e282e6ee5
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-12 15:22:59 +0600

    Convert boolean values to strings 'Yes' or 'No'.

---
+++ 8_kyu/problem_4.js
@@ -0,0 +1,46 @@
+/* 
+    Convert boolean values to strings 'Yes' or 'No'.
+*/
+
+//Best Cases
+function boolToWord(bool) {
+    return bool ? "Yes" : "No";
+}
+
+
+/*
+function boolToWord( bool ){
+ return bool === true ? "Yes" : "No";
+}
+*/
+
+/*
+function boolToWord( bool ){
+ return bool === true || false ? "Yes" : "No";
+}
+ 
+*/
+
+/*
+ function boolToWord(bool){
+ if(bool === true){
+ return "Yes";
+ }
+ if(bool === false){
+ return "No"
+ }
+ }
+ 
+*/
+
+
+/*
+function boolToWord( bool ){
+  if(bool === true){
+ return "Yes";
+ }
+ else{
+ return "No"
+ }
+}
+*/
