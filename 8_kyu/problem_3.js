5d82bcfbed454c4c183671a7f8a20e79bcb48372
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-12 15:21:09 +0600

    Calculate BMI

---
+++ 8_kyu/problem_3.js
@@ -0,0 +1,38 @@
+/*
+    Calculate BMI
+ */
+function bmi(weight, height) {
+    const result = weight / Math.pow(height, 2);
+    if (result <= 18.5) {
+        return "Underweight";
+    }
+    if (result <= 25.0) {
+        return "Normal";
+    }
+    if (result <= 30) {
+        return "Overweight"
+    }
+    if (result > 30) {
+        return "Obese";
+    }
+}
+
+
+
+/* 
+ function bmi(weight, height) {
+    const result = weight / Math.pow(height, 2);
+    if (result <= 18.5) {
+        return "Underweight";
+    }
+    if (result <= 25.0) {
+        return "Normal";
+    }
+    if (result <= 30) {
+        return "Overweight"
+    }
+    else{
+        return "Obese";
+    }
+}
+*/
