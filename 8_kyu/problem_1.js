3cdce053b092f428466ae593abca3ea1c40d1d93
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-12 15:17:25 +0600

    Beginner Series #1 School Paperwork

---
+++ 8_kyu/problem_1.js
@@ -0,0 +1,20 @@
+/* 
+    Beginner Series #1 School Paperwork
+*/
+function paperwork(n, m) {
+    if (n < 0 || m < 0) {
+        return 0;
+    }
+    else {
+        return n * m;
+    }
+}
+
+
+
+/*
+  // Best cases
+    function paperwork(n, m) {
+    return n < 0 || m < 0 ? 0 : n * m;
+}
+*/
