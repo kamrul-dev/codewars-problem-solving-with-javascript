8c3450e40f84dbf56aeed1a11d2011febb7cf9cd
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-12 15:46:22 +0600

    You only need one - Beginner

---
+++ problem_6.js
@@ -0,0 +1,32 @@
+/* 
+    You only need one - Beginner
+*/
+
+function check(a, x) {
+    if (a.includes(x)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+/* 
+    //Very usefull
+    function check(a,x){
+        return a.indexOf(x) > -1 ? true : false;
+};
+*/
+
+/* 
+    // Best Practices
+    const check = (a,x) => a.includes(x);
+*/
+
+
+/* 
+function check(a,x){
+  return a.includes(x);
+};
+*/
+
